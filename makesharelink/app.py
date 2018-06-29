064a249c747566803fbc9ee9ac24c6e218cc99cc
Tiago Cordeiro <tiago@mulhergorila.com>
2018-06-29 17:01:17 -0300

    Adiciona versão para produto promocional, preço antigo, preço novo

--- makesharelink/app.py
+++ makesharelink/app.py
@@ -17,18 +17,25 @@ def create_app():
         product = r.html.find('.heading-title', first=True)
         image = r.html.find('#image', first=True)
         price = r.html.find('.product-price', first=True)
+        price_old = r.html.find('.price-old', first=True)
+        price_new = r.html.find('.price-new', first=True)
         description = r.html.find('#tab-description', first=True)
         options = r.html.find('.options', first=True)
-        if options:
-            pricevalue = 'a partir de ' + price.text
-        else:
-            pricevalue = price.text
 
-        print(product.text)
-        print(image.attrs['src'])
-        print(price.text)
-        print(description.text[:896] + '...')
-        print('saiba mais ' + url)
+        if price:
+            if options:
+                pricevalue = 'a partir de ' + price.text
+            else:
+                pricevalue = price.text
+
+        if price_new and price_old:
+            pricevalue = 'de ' + price_old.text + ' por ' + price_new.text
+        
+        # print(product.text)
+        # print(image.attrs['src'])
+        # print(price.text)
+        # print(description.text[:896] + '...')
+        # print('saiba mais ' + url)
         return render_template('product.html', url=url, product=product.text,
                                image=image.attrs['src'], price=pricevalue,
                                description=description.text[:896] + '...')
